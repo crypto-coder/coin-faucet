b895994a54640bf72b22eb99a472b931e8f9ad6d
crypto-coder <chris.williams@puremoneysystems.com>
2018-09-23 12:40:55 -0400

    Updated RasPi install process

--- raspi-rchain/openSUSE/step1_DISABLE-GUI.sh
+++ raspi-rchain/openSUSE/step1_DISABLE-GUI.sh
@@ -2,3 +2,15 @@
 
 # Disable the GUI, by setting the default to run-level 3 (multi-user)
 systemctl set-default multi-user.target
+
+# Make sure the /etc/bash.bashrc.local file exists
+if [ ! -f /etc/bash.bashrc.local ]; then
+    touch /etc/bash.bashrc.local
+fi
+
+# Check if wifi power_save off command is already in the bash.bashrc.local file
+power_save_count=$(grep -e "power_save" -c /etc/bash.bashrc.local)
+
+if [ $power_save_count = 0 ]; then
+    echo "iw dev wlan0 set power_save off" >> /etc/bash.bashrc.local
+fi
